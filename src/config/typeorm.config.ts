989491b1ed702c7eb252e6b60008b605b67114dd
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-03-02 14:23:09 -0400

    feat: Production config to run migrations

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,15 +9,17 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: this.configService.get<any>('DB_TYPE'),
+      synchronize: JSON.parse(this.configService.get<string>('DB_SYNCHRONIZE')),
       host: this.configService.get<string>('DB_HOST'),
       port: this.configService.get<number>('DB_PORT'),
       username: this.configService.get<string>('DB_USERNAME'),
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_DATABASE'),
       autoLoadEntities: true,
-      migrationsRun: this.configService.get<boolean>('MIGRATIONS_RUN'),
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      migrationsRun: JSON.parse(
+        this.configService.get<string>('DB_MIGRATIONS_RUN'),
+      ),
     };
   }
 }
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,16 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+require('dotenv').config();
 
 export const appDataSource = new DataSource({
-  type: 'postgres',
-  database: 'ecommerce',
-  entities: ['**/*.entity{.js,.ts}'],
-  migrations: [__dirname + '/migrations/*{.js,.ts}'],
+  type: process.env.DB_TYPE,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrationsRun: true,
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 } as DataSourceOptions);
